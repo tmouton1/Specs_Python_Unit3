b081a0ad38c1227479bab0f50f96f048e7818a94
tmouton1 <tamia.mouton1@gmail.com>
2023-01-28 22:56:13 -0500

    full site funcitonal

---
+++ melons.py
@@ -0,0 +1,46 @@
+import csv
+
+class Melon:
+   def __init__(self,melon_id,common_name,price,image_url,color,seedless,):
+      self.melon_id = melon_id
+      self.common_name = common_name
+      self.price = price
+      self.image_url = image_url
+      self.color = color
+      self.seedless = seedless
+
+   def __repr__(self):
+    return (f"<Melon: {self.melon_id}, {self.common_name}>")
+   
+   def price_str(self):
+    return f"${self.price:.2f}"
+
+melon_dict = {}
+
+with open("melons.csv") as csvfile:
+   rows = csv.DictReader(csvfile)
+   for row in rows:
+      #  print(row)
+
+    melon_id = row['melon_id']
+   
+    melon = Melon(melon_id,row['common_name'],float(row['price']),row['image_url'],row['color'],eval(row['seedless']))
+
+    melon_dict[melon_id] = melon
+
+print(melon_dict)
+
+# ==================
+
+def get_by_id(melon_id):
+    return melon_dict[melon_id]
+
+def get_all():
+    return list(melon_dict.values())
+
+
+
+
+
+
+
